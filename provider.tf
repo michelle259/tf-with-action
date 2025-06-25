6aef6bca6851b56a044080b095b533a248a43f94
yeeun <yeeun@yeeunui-MacBookPro.local>
2025-06-25 13:12:11 +0900

    fix *.tf

--- provider.tf
+++ provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
   backend "s3" {
-	  bucket         = "backend-bucket-ye"
-	  key            = "terraform/state-test/terraform.tfstate"
-	  region         = "ap-northeast-2"
-      dynamodb_table = "terraform-lock"
-	}
+    bucket         = "backend-bucket-ye"
+    key            = "terraform/state-test/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
