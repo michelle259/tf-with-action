2abae31c71a795e4403edbc9cc305c742354cd19
yeeun <yeeun@yeeunui-MacBookPro.local>
2025-06-25 15:29:30 +0900

    update cloud configuration

--- main.tf
+++ main.tf
@@ -2,18 +2,7 @@ resource "aws_vpc" "backend-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "tf-backend-plan"
+    Name = "tf-backend-cloud"
   }
 }
 
-module "backend" {
-  source = "./modules/backend"
-}
-
-module "DynamoDB" {
-  source = "./modules/dynamodb"
-}
-
-module "s3" {
-  source = "./modules/s3"
-}
--- provider.tf
+++ provider.tf
@@ -5,12 +5,22 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  backend "s3" {
-    bucket         = "backend-bucket-ye"
-    key            = "terraform/state-test/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
-  }
+    cloud { 
+    
+    organization = "tfc-devops-yeeun" 
+
+    workspaces { 
+      name = "tf-devops" 
+    } 
+  } 
+
+  
+#   backend "s3" {
+#     bucket         = "backend-bucket-ye"
+#     key            = "terraform/state-test/terraform.tfstate"
+#     region         = "ap-northeast-2"
+#     dynamodb_table = "terraform-lock"
+#   }
 }
 
 provider "aws" {
